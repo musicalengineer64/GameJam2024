51d22d0f73765ca07788894019111c91848c2af0
MusicalEngineer <musicalengineer64@gmail.com>
2024-07-20 01:19:08 -0400

    Summon and Clear Enemies

--- scenes/scene_scripts/EnemySample.gd
+++ scenes/scene_scripts/EnemySample.gd
@@ -1,34 +1,50 @@
 extends Node2D
 
-var file_path = "res://enemy_sample.json"
+#@export var enemy_sample: PackedScene
+
+var file_path = "res://enemies/enemy_sample.json"
 var json_as_text = FileAccess.get_file_as_string(file_path)
 var json_as_dict = JSON.parse_string(json_as_text)
 
 var base_health = json_as_dict["enemy_base_health"]
 var base_priority = json_as_dict["enemy_priorities"]["full_health"]
 
-var priority = null
-var health = null
+var priority_label = null
+var health_label = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	print(json_as_dict)
 	print(base_health)
 	
-	priority = get_node("EnemyPriority")
-	health = get_node("EnemyHealthLabel")
+	priority_label = get_node("EnemyPriorityLabel")
+	health_label = get_node("EnemyHealthLabel")
 	
-	health.text = str(base_health)
-	priority.text = str(base_priority)
+	priority_label.text = str(base_priority)
+	health_label.text = str(base_health)
 
+# Modify the health value of the enemy
+func _take_damage(amount=10):
+	var health_val = int(health_label.text)
+	var priority_val = str(priority_label.text)
+	
+	if health_val < amount:
+		health_val = 0
+	else:
+		health_val -= amount
+	
+	# change the enemy behavior based on their new health
+	if health_val < 50:
+		priority_val = json_as_dict["enemy_priorities"]["medium_health"]
+	if health_val < 20:
+		priority_val = json_as_dict["enemy_priorities"]["low_health"]
+	if health_val == 0:
+		get_node("EnemySprite").texture = preload("res://textures/enemy/PlaceholderEnemy2.png")
+		
+	# update labels
+	health_label.text = str(health_val)
+	priority_label.text = str(priority_val)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if int(self.get_child(1).text) < 50:
-		priority.text = json_as_dict["enemy_priorities"]["medium_health"]
-		#self.get_child(2).text = str(priority)
-	if int(self.get_child(1).text) < 20:
-		priority.text = json_as_dict["enemy_priorities"]["low_health"]
-		#self.get_child(2).text = str(priority)
-	if int(self.get_child(1).text) == 0:
-		get_node("EnemySprite").texture = preload("res://Textures/PlaceholderEnemy2.png")
+	pass
--- scenes/scene_scripts/presenter.gd
+++ scenes/scene_scripts/presenter.gd
@@ -1,9 +1,70 @@
 extends Node2D
 
+var damage_button = Button.new()
+var summon_button = Button.new()
+var clear_button = Button.new()
+
+var enemy_list = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	# Create a button in presenter to demonstrate damaging an enemy
+	damage_button.text = "Damage Enemy"
+	damage_button.position = Vector2(100,100)
+	damage_button.pressed.connect(self._damage_button_pressed)
+	add_child(damage_button)
+	
+	# Create a button in presenter to demonstrate summoning an enemy
+	summon_button.text = "Summon Enemy"
+	summon_button.position = Vector2(100,200)
+	summon_button.pressed.connect(self._summon_button_pressed)
+	add_child(summon_button)
+	
+	clear_button.text = "Clear Enemy"
+	clear_button.position = Vector2(100,300)
+	clear_button.pressed.connect(self._clear_button_pressed)
+	add_child(clear_button)
+
+# damage an enemy
+func _damage_button_pressed():
+	for enemy in enemy_list:
+		enemy._take_damage()
+
+# place enemies on the board
+func _summon_button_pressed():
+	if enemy_list.size() == 0:
+		enemy_list.push_back(load("res://scenes/enemy_sample.tscn").instantiate())
+		enemy_list[0].position = Vector2(700,300)
+		add_child(enemy_list[0])
+	elif enemy_list.size() == 1:
+		enemy_list.push_back(load("res://scenes/enemy_sample.tscn").instantiate())
+		enemy_list[0].position = Vector2(600,200)
+		enemy_list[1].position = Vector2(800,400)
+		add_child(enemy_list[1])
+	elif enemy_list.size() == 2:
+		enemy_list.push_back(load("res://scenes/enemy_sample.tscn").instantiate())
+		enemy_list[0].position = Vector2(750,200)
+		enemy_list[1].position = Vector2(900,400)
+		enemy_list[2].position = Vector2(600,400)
+		add_child(enemy_list[2])
+	elif enemy_list.size() == 3:
+		print("Board is Full!")
+
+func _clear_button_pressed():
+	if enemy_list.size() == 0:
+		print("I have no enemies.")
+	elif enemy_list.size() == 1:
+		enemy_list[-1].free()
+		enemy_list.pop_back()
+	elif enemy_list.size() == 2:
+		enemy_list[-1].free()
+		enemy_list.pop_back()
+		enemy_list[0].position = Vector2(700,300)
+	elif enemy_list.size() == 3:
+		enemy_list[-1].free()
+		enemy_list.pop_back()
+		enemy_list[0].position = Vector2(600,200)
+		enemy_list[1].position = Vector2(800,400)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
